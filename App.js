99bced596eebac7cbca07bdb2dd45d6020520c09
unknown <felipe.silvamachado@yahoo.com>
2025-08-18 22:34:29 -0300

    Subindo o exercicio de FlatList

---
+++ App.js
@@ -0,0 +1,126 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Sua HQ aqui!</Text>
+      <StatusBar style="auto" />
+      <FlatList
+        data={hqs}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Image source={item.img} style={styles.img} />
+            <Text style={styles.titulo}>{item.titulo}</Text>
+            <Text style={styles.titulo}>{item.ano}</Text>
+            <Text style={styles.titulo}>{item.editora}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E0FFFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titulo: {
+    fontFamily: 'Arial',
+    fontSize: 16,
+    textAlign: 'center',
+    alignItems: 'center',
+    color: '#ffffff',
+    margin: 3,
+  },
+  img: {
+    width: 350,
+    height: 350,
+    resizeMode: 'cover',
+    borderRadius: 5,
+  },
+  item: {
+    padding: 5,
+    margin: 5,
+    backgroundColor: '#000000',
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+});
+
+const hqs = [
+  {
+    id: 1,
+    titulo: 'Homem - Aranha',
+    ano: 2015,
+    editora: 'Marvel Comics',
+    img: require('./assets/homemAranha.png'),
+  },
+  {
+    id: 2,
+    titulo: "Hulk contra todos!",
+    ano: 2012,
+    editora: "Marvel Comics",
+    img: require('./assets/hulk.png'),
+  },
+  {
+    id: 3,
+    titulo: "Vingadores Avante",
+    ano: 2005,
+    editora: "Marvel Comics",
+    img: require('./assets/vingadores.png'),
+  },
+  {
+    id: 4,
+    titulo: "Homem de Ferro",
+    ano: 2018,
+    editora: "Marvel Comics",
+    img: require('./assets/homemDeFerro.png'),
+  },
+  {
+    id: 5,
+    titulo: "Capitão América - Hydra",
+    ano: 1985,
+    editora: "Marvel Comics",
+    img: require('./assets/capitaoAmerica.png'),
+  },
+  {
+    id: 6,
+    titulo: "Batman - Gothan City",
+    ano: 2009,
+    editora: "DC Comics",
+    img: require('./assets/batman.png'),
+  },
+  {
+    id: 7,
+    titulo: "Superman!",
+    ano: 2012,
+    editora: "DC Comics",
+    img: require('./assets/superman.png'),
+  },
+  {
+    id: 8,
+    titulo: "Liga da Justiça",
+    ano: 2015,
+    editora: "DC Comics",
+    img: require('./assets/ligaDaJustica.png'),
+  },
+  {
+    id: 9,
+    titulo: "Flash",
+    ano: 2010,
+    editora: "DC Comics",
+    img: require('./assets/flash.png'),
+  },
+  {
+    id: 10,
+    titulo: "Lanterna Verde",
+    ano: 2001,
+    editora: "DC Comics",
+    img: require('./assets/lanternaVerde.png'),
+  },
+]
